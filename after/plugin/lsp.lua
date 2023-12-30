ff0d8893fc34d3613e6d5c36e57e75608ea87266
ninbura <gabriel@ninbura.com>
2023-12-30 00:40:33 -0700

    updated lsp config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -8,7 +8,7 @@ lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
-require('mason').setup({})
+require('mason').setup()
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here 
   -- with the ones you want to install
